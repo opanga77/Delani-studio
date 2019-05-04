fc7e114f6d494e4643cc828787864b6da5a3909c
thepsalmist <xf.xavierfrank@gmail.com>
2019-05-04 15:50:08 +0300

    create functions to toggle in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#devLogo").click(function() {
+    $(".toggleLogo").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#productLogo").click(function() {
+    $(".toggleProductLogo").toggle();
+  });
+});
