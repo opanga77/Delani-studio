e7131b20df640dfa56ecf8323cab7663046cb84d
thepsalmist <xf.xavierfrank@gmail.com>
2019-05-05 21:01:04 +0300

    modify js function to display message on submitting form

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,3 @@
-var email = $('#exampleFormControlInput1').val();
-var name = $('#nameInput').val();
-var message = $('#messageInput').val();
-var statusElm = $('#status');
-statusElm.empty();
-if (email.length > 5 && email.includes('@') && email.includes('.')) {
-  console.log("email valid")
-} else {
-  console.log("email invalid")
-}
-if (name.length > 2) {
-  console.log(name);
-} else {
-  console.log('name invalid')
-}
-if (message.length > 2) {
-  statusElm.html('< div > Thank you for your message" < /div>');
-} else {
-  statusElm.html("Cool");
-}
-
 $(document).ready(function() {
   $("#designLogo").click(function() {
     $(".toggleDesignLogo").toggle();
@@ -37,6 +16,26 @@ $(document).ready(function() {
   $('.submit').click(function(event) {
     event.preventDefault();
     console.log('clicked button');
+    var name = document.getElementById('nameInput').value;
+    var email = document.getElementById('exampleFormControlInput1').value;
+    var message = document.getElementById('messageInput').value;
+    var statusElm = document.getElementById('status');
 
+    if (email.length > 5 && email.includes('@') && email.includes('.')) {
+      console.log("email valid")
+    } else {
+      console.log("email invalid")
+    }
+    if (name.length > 2) {
+      console.log(name);
+    } else {
+      console.log('name invalid')
+    }
+    if (message.length > 2) {
+      document.getElementById('status').innerText = "Thank you for your message!"
+    } else {
+      document.getElementById('status').innerText = "Null"
+    }
   });
+
 });
