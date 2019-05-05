1cdb1f177915561d2bc936291a1c31167a48dacb
thepsalmist <xf.xavierfrank@gmail.com>
2019-05-05 11:42:44 +0300

    modify js file to display output in html by creating empty div (statu)s

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,42 @@
+var email = $('#exampleFormControlInput1').val();
+var name = $('#nameInput').val();
+var message = $('#messageInput').val();
+var statusElm = $('#status');
+statusElm.empty();
+if (email.length > 5 && email.includes('@') && email.includes('.')) {
+  console.log("email valid")
+} else {
+  console.log("email invalid")
+}
+if (name.length > 2) {
+  console.log(name);
+} else {
+  console.log('name invalid')
+}
+if (message.length > 2) {
+  statusElm.html('< div > Thank you for your message" < /div>');
+} else {
+  statusElm.html("Cool");
+}
+
 $(document).ready(function() {
   $("#designLogo").click(function() {
     $(".toggleDesignLogo").toggle();
   });
-});
 
-$(document).ready(function() {
   $("#devLogo").click(function() {
     $(".toggleDevLogo").toggle();
-
   });
-});
 
-$(document).ready(function() {
   $("#productLogo").click(function() {
     $(".toggleProductLogo").toggle();
+  });
+});
+console.log('it works')
+$(document).ready(function() {
+  $('.submit').click(function(event) {
+    event.preventDefault();
+    console.log('clicked button');
+
   });
 });
